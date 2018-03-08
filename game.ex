b944ecd6182ae02fbdc197f18a894a8d8e4f13a3
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-03-08 11:14:01 -0600

    play a game between 2 easy AIs
    make variable name more expressive
    
    create a folder for players
    
    pass in players and symbols to play()
    
    remove move()
    
    cell_selection(_) needs an arg to match AI's implementation
    
    give a default boardsize of 9
    
    validate user cell selection input
    
    validate human player's cell selection

--- game.ex
+++ game.ex
@@ -1,10 +1,18 @@
 defmodule Game do
-  def play(board, player_one, player_two) do
-    cell_index = EasyAI.cell_selection(board)
-    board |> move(cell_index, player_one)
-  end
+  def play(board, players, symbols) do
 
-  def move(board, cell, player) do
-    Board.insert_symbol(board, cell, player)
+    CLI.display_board(board)
+
+    if Rules.game_in_progress?(board) do
+      current_player = Enum.at(players, 0)
+      current_player_symbol = Enum.at(symbols, 0)
+
+      cell_index = current_player.cell_selection(board)
+      new_board = board |> Board.insert_symbol(cell_index, current_player_symbol)
+
+      play(new_board, Enum.reverse(players), Enum.reverse(symbols))
+    else
+      board
+    end
   end
 end
--- human_player.ex
+++
@@ -1,8 +0,0 @@
-defmodule HumanPlayer do
-
-  def cell_selection() do
-    cell = IO.gets "please enter your cell selection\n"
-    cell |> String.trim |> String.to_integer
-  end
-
-end
