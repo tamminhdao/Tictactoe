01de9872a7b9b192848101d268b4424a024a681f
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-03-09 11:57:47 -0600

    empty master branch

---
+++ lib/tictactoe.ex
@@ -0,0 +1,18 @@
+defmodule Tictactoe do
+  @moduledoc """
+  Documentation for Tictactoe.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tictactoe.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
